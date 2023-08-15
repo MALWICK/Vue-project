dd5b3be3f9648d6f9780aac62f75c191a57bc219
MALWICK <batahumphrey66@gamil.com>
2023-08-15 12:51:18 +0100

    created button

---
+++ src/components/Button.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="button-holder">
+<button class="My-add" type="button">Add Task</button>
+    </div>
+</template>
+
+<script>
+export default  {
+    name: 'add-Button',
+}
+</script>
