1b0bc0f94b2f6ed84955cccc576b2528c6a60eef
MALWICK <batahumphrey66@gamil.com>
2023-08-15 15:20:04 +0100

    creating tassks

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,71 @@
 <template>
   <div class="container">
     <Headers title="Task Tracker" />
+    <Tasks :tasks="tasks"/>
   </div>
 </template>
 
 <script>
+
 import Headers from './components/Header.vue'
+import Tasks from './components/Tasks.vue'
+
 export default {
   name: 'App',
   components: {
-    Headers
+    Headers,
+    Tasks,
   },
   data() {
     return {
       tasks: [],
     }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Learn Vue',
+        day: 'March 1st at 2:30pm',
+        reminder: true
+      },
+      {
+        id: 2,
+        text: 'Meet at school',
+        day: 'March 2nd at 2:00pm',
+        reminder: false,
+      },
+      {
+        id: 3,
+        text: 'Buy Groceries',
+        day: 'March 3rd at 12:30pm',
+        reminder: true
+      },
+      {
+        id: 4,
+        text: 'Learn Vue',
+        day: 'March 4th at 10:30am',
+        reminder: false
+      },
+      {
+        id: 5,
+        text: 'Take out the the dog for a walk',
+        day: 'March 5th at 1:00pm',
+        reminder: true
+      },
+      {
+        id: 6,
+        text: 'Visit grand Mom',
+        day: 'March 6th at 2:00pm',
+        reminder: false
+      }
+    ]
   }
 }
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
 .container {
   max-width: 500px;
   margin: 30px auto;
---
+++ src/components/Tasks.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="my-task">
+        <h1>My Tasks</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "My-Tasks",
+    props: {
+        tasks: Array
+    }
+}
+</script>
